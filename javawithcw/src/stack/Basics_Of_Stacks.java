699d375d683627baaeb0c2da89f0377f0253b2a5
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-02-13 00:31:22 +0530

    main.java

--- javawithcw/src/stack/Basics_Of_Stacks.java
+++ javawithcw/src/stack/Basics_Of_Stacks.java
@@ -1,5 +1,17 @@
 package stack;
 
+import java.util.Stack;
+
 public class Basics_Of_Stacks {
 
+    public static void main(String[] args) {
+        Stack<Integer> st =new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st.peek());
+        System.out.println(st);
+    }
 }
