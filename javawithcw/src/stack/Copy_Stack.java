9aebb3e6526f244a46b26873296c80047ca569e0
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-02-14 00:53:32 +0530

    copying the data of stack1 into stack2 and try to printing that in reverse order in Copy_Stack.java

--- javawithcw/src/stack/Copy_Stack.java
+++ javawithcw/src/stack/Copy_Stack.java
@@ -1,5 +1,26 @@
 package stack;
 
-public class Copy_Stack {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Copy_Stack {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Stack<Integer> st=new Stack<>();
+        Stack<Integer> st1=new Stack<>();
+        int n;
+        System.out.println("Enter The Number Of Elements you want in Stack");
+        n=sc.nextInt();
+        while(n>0){
+            st.push(sc.nextInt());
+            n--;
+        }
+        System.out.println("Your Stack : "+st);
+        while(st.size()>0){
+            st1.push(st.peek());
+            st.pop();
+        }
+        System.out.println("Copied Stack : "+st1);
+
+    }
 }
