56aef06c5bb16ffab94dd20f3f49378d36970e34
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-02-14 23:47:11 +0530

    In Display_Stack_Using_Array.java we going to Displaying Stack without using sout(st); with the help of arrays

---
+++ javawithcw/src/stack/Display_Stack_Using_Array.java
@@ -0,0 +1,32 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Display_Stack_Using_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> st = new Stack<>();
+        System.out.println("Enter the Number of element you want in a stack");
+        int n;
+         n = sc.nextInt();
+        int p=n;
+        System.out.println("Enter the element :");
+        while (p > 0) {
+            st.push(sc.nextInt());
+            p--;
+        }
+        System.out.println(st);
+        int[] myarr = new int[st.size()];
+        for (int i = (n - 1); i >= 0; i--) {
+            myarr[i] = st.pop();
+        }
+        for (int j=0; j <n; j++) {
+            System.out.print(myarr[j]+" ");
+            st.push(myarr[j]);
+        }
+        System.out.println("");
+        System.out.println(st);
+
+    }
+}
