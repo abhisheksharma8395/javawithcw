27b671d013ccf93377ad25e5a3316b363a97a532
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-02-15 01:18:22 +0530

    In Displaying_Stack_Using_Recursion.java we going to Displaying Stack without using sout(st); with the help of recursion

---
+++ javawithcw/src/stack/Displaying_Stack_Using_Recursion.java
@@ -0,0 +1,29 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Displaying_Stack_Using_Recursion {
+    public static void display_recursively(Stack<Integer> st1) {
+        if(st1.size()==0) return;
+        int x = st1.pop();
+        System.out.print(x+" ");
+        display_recursively(st1);
+        st1.push(x);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> st = new Stack<>();
+        System.out.println("Enter the Number of element you want in a stack");
+        int n;
+        n = sc.nextInt();
+        int p = n;
+        System.out.println("Enter the element :");
+        while (p > 0) {
+            st.push(sc.nextInt());
+            p--;
+        }
+        display_recursively(st);
+    }
+}
