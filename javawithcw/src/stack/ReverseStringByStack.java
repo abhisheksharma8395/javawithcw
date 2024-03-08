df4d30940aa4cc3d0a56cd9d7b8394ee0f5b8702
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-03-08 20:06:16 +0530

    In ReverseStringByStack.java we are going to reverse String by using Stack

---
+++ javawithcw/src/stack/ReverseStringByStack.java
@@ -0,0 +1,24 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseStringByStack {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        Stack<Character> st=new Stack();
+        System.out.println("Enter your String");
+        String a=sc.next();
+        int idx=0;
+        while(idx<a.length()){
+            char ch=a.charAt(idx);
+            st.push(ch);
+            idx++;
+        }
+        StringBuilder str= new StringBuilder();
+        while(st.size()!=0){
+            str.append(st.pop());
+        }
+        System.out.println("Reversed String is "+str);
+    }
+}
