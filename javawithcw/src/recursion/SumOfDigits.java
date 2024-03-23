b5b1a14fd23636dfb521fa8deeb729494f9e04db
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-03-24 01:22:03 +0530

    In SumOfDigits.java we are going to print Sum of Digits in number using Recursion.

---
+++ javawithcw/src/recursion/SumOfDigits.java
@@ -0,0 +1,19 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    static int sumofdigit(int n){
+        if(n==0){
+            return 0;
+        }
+        return ((n%10)+(sumofdigit(n/10)));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sumofdigit(n));
+    }
+
+}
