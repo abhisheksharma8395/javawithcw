14f84cba6dba560ba5e0f579a1f7c3d7014546f8
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-03-24 12:45:14 +0530

    In CountOfDigitsByRecursion.java we are going to print total number of digits in number using Recursion.

--- javawithcw/src/recursion/CountOfDigitsByRecursion.java
+++ javawithcw/src/recursion/CountOfDigitsByRecursion.java
@@ -2,13 +2,10 @@ package recursion;
 import java.util.Scanner;
 public class CountOfDigitsByRecursion {
     public static int countofdigits(int val){
-        int count = 0;
         if(val>=0 && val<=9){
             return 1;
         }
-
         return 1+countofdigits(val/10);
-
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
