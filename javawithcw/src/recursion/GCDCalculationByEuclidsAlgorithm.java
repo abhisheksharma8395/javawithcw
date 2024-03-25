272a27012f5afb3a1b6ebc75be47af80a0a9f561
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-03-26 02:01:17 +0530

    In GCDCalculationByEuclidsAlgorithm.java we are going to calculate GCD by using euclid algorithm and recursion.

--- javawithcw/src/recursion/GCDCalculationByEuclidsAlgorithm.java
+++ javawithcw/src/recursion/GCDCalculationByEuclidsAlgorithm.java
@@ -2,17 +2,19 @@ package recursion;
 
 import java.util.Scanner;
 
-public class GCDCalculationByLongestDivisionMethod {
-    public static int ldgcdr(int val1,int val2){
+public class GCDCalculationByEuclidsAlgorithm {
+    public static int eagcdr(int val1,int val2){
         if(val1%val2==0){
             return val2;
         }
-        return ldgcdr(val2,val1%val2);
+        //Euclid Algorithm
+        //
+        return eagcdr(val2,val1%val2);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int val1 = scanner.nextInt();
         int val2 = scanner.nextInt();
-        System.out.println(ldgcdr(val1,val2));
+        System.out.println(eagcdr(val1,val2));
     }
 }
