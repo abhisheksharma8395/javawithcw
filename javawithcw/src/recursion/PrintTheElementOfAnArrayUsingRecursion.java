9279840f71143836a9fa9f210c180f950d231f64
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-03-27 14:48:05 +0530

    In PrintTheElementOfAnArrayUsingRecursion.java we are going to print the elements in an array by using recursion.

--- javawithcw/src/recursion/PrintTheElementOfAnArrayUsingRecursion.java
+++ javawithcw/src/recursion/PrintTheElementOfAnArrayUsingRecursion.java
@@ -3,13 +3,13 @@ public class PrintTheElementOfAnArrayUsingRecursion {
     public static void printbyrec(int[] arr,int idx){
         //Base Case
         if(idx==0){
-            System.out.println(arr[0]);
+            System.out.print(arr[0]+" ");
             return;
         }
         //Subproblems
         printbyrec(arr,idx-1);
         //Self Work
-        System.out.println(arr[idx]);
+        System.out.println(arr[idx]+" ");
     }
     public static void main(String[] args) {
         int[] arr = new int[]{8,9,4,0,3,7};
