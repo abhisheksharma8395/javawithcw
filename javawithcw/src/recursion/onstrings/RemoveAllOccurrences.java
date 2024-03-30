6279650554448fe255acbffa119684455efe4eeb
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-03-30 16:45:17 +0530

    In RemoveAllOccurrences.java we are going to find character in string and remove all its occurrence and return that string by help of recursion.

---
+++ javawithcw/src/recursion/onstrings/RemoveAllOccurrences.java
@@ -0,0 +1,23 @@
+package recursion.onstrings;
+import java.util.Scanner;
+public class RemoveAllOccurrences {
+    //remove all the occurrences of a from string abcax
+    public static String removeOccurrence(String s,char remove,int idx){
+        String a = "";
+        if(idx == s.length()){          //Base Case
+            return a;
+        }
+        char currentCharacter = s.charAt(idx);
+        if(currentCharacter != remove){       //Self Work
+            a=a+currentCharacter;
+        }
+        String small = removeOccurrence(s,remove,idx+1);  //Subproblem
+        return a + small;                           //Self Work + Subproblem
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        char remove = scanner.next().charAt(0);
+        System.out.println(removeOccurrence(s,remove,0));
+    }
+}
