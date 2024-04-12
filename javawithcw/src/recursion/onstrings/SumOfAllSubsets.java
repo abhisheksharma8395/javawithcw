964fc86d1b1e276c1460186ce55ec3c42f6d3a61
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-04-13 00:10:20 +0530

    In SumOfAllSubsets.java we are going to find sum of all subarrays of array by help of recursion.

--- javawithcw/src/recursion/onstrings/SumOfAllSubsets.java
+++ javawithcw/src/recursion/onstrings/SumOfAllSubsets.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class SumOfAllSubsets {
     private static void SumOfSubsets(int[] arr, int sum, int idx) {
         if (idx >= arr.length) {
-            System.out.println(sum);
+            System.out.print(sum+" ");
             return;
         }
         int num = arr[idx];
-        SumOfSubsets(arr, sum + num, idx + 1);
-        SumOfSubsets(arr, sum, idx + 1);
+        SumOfSubsets(arr, sum + num, idx + 1);        //include
+        SumOfSubsets(arr, sum, idx + 1);                   //exclude
     }
 
     public static void main(String[] args) {
