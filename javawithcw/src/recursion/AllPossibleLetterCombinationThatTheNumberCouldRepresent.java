8390668ec509e2329e6a9eb89bd8e77ed5472cb3
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-04-13 19:28:25 +0530

    In AllPossibleLetterCombinationThatTheNumberCouldRepresent.java we are going to solve Given a String containning digit 2-9, return all possible letter combination that the number could represent.Return the answer in any order by help of recursion.

---
+++ javawithcw/src/recursion/AllPossibleLetterCombinationThatTheNumberCouldRepresent.java
@@ -0,0 +1,26 @@
+package recursion;
+
+import java.util.Scanner;
+//Given a String containning digit 2-9, return all possible letter combination that the
+// number could represent.Return the answer in any order
+public class AllPossibleLetterCombinationThatTheNumberCouldRepresent {
+    private static void possibleCombination(String digit, String res, String[] mapping) {
+        if (digit.isEmpty()) {
+            System.out.println(res);
+            return;
+        }
+        int num = digit.charAt(0) - '0';
+        String currentString = mapping[num];
+        for (int i = 0; i < currentString.length(); i++) {
+            possibleCombination(digit.substring(1), res + currentString.charAt(i), mapping);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        possibleCombination(s, "", mapping);
+    }
+}
+
