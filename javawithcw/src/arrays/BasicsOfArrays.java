471cc597ff2695116cc45ca1f54d5f4b58474bc7
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-06 00:09:43 +0530

    accesing the element of multidimensional array

--- javawithcw/src/arrays/BasicsOfArrays.java
+++ javawithcw/src/arrays/BasicsOfArrays.java
@@ -3,6 +3,12 @@ package arrays;
 import java.sql.SQLOutput;
 
 class ArrayExample{
+    void multidimensonal(){
+        int[][] arr = {{1,4,2},{1,3,4},{7,5,6},{4,9,2}};
+        int[][][] array = {{{1,2},{2,4}},{{4,5},{6,2}}};
+        System.out.println(arr[2][2]);
+        System.out.println(array[1][1][0]);
+    }
     void demoArray(int a){
         int[] arr = {4,5,3,2,7,8};
         float[] weight = {23.57f,24.63f,89.26f,20.38f,78.22f,96.88f};
@@ -17,5 +23,6 @@ public class BasicsOfArrays {
         ArrayExample arrayExample = new ArrayExample();
         int a = 5;
         arrayExample.demoArray(a);
+        arrayExample.multidimensonal();
     }
 }
