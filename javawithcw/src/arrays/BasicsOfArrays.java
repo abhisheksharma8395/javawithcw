2def46245d432e90e855a05c346e5f80c664bbfd
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-05 19:58:40 +0530

    accesing the element of float array and string array

--- javawithcw/src/arrays/BasicsOfArrays.java
+++ javawithcw/src/arrays/BasicsOfArrays.java
@@ -1,8 +1,15 @@
 package arrays;
+
+import java.sql.SQLOutput;
+
 class ArrayExample{
     void demoArray(int a){
         int[] arr = {4,5,3,2,7,8};
+        float[] weight = {23.57f,24.63f,89.26f,20.38f,78.22f,96.88f};
+        String[] name = {"Hathras","Hardoi","hamirpur","Agra","Etah","Mathura"};
         System.out.println(arr[a]);
+        System.out.println(weight[a]);
+        System.out.println(name[a]);
     }
 }
 public class BasicsOfArrays {
