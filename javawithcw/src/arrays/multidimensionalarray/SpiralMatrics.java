218f5822b09bc020d3ff5a211b8e834a686d0922
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-20 18:45:35 +0530

    printing Matrix in spiral order.

--- javawithcw/src/arrays/multidimensionalarray/SpiralMatrics.java
+++ javawithcw/src/arrays/multidimensionalarray/SpiralMatrics.java
@@ -3,31 +3,24 @@ package arrays.multidimensionalarray;
 import java.util.Scanner;
 
 public class SpiralMatrics {
-//    public static void Print2DArray(int[][] array) {
-//        for (int[] i : array) {
-//            for (int j : i) {
-//                System.out.print(j + " ");
-//            }
-//            System.out.println(" ");
-//        }
-//    }
-    public static void SpiralMatricsMaker(int[][] array,int row) {
+    public static void SpiralMatricsMaker(int[][] array, int row) {
 
-        for (int i = 0; i <= row/2; i++) {
-            for (int j = i; j <= row-1-i; j++) {
-                System.out.print(array[i][j]+" ");
+        for (int i = 0; i <= row / 2; i++) {
+            for (int j = i; j <= row - 1 - i; j++) {
+                System.out.print(array[i][j] + " ");
             }
-            for (int j = i+1; j <= row-1-i ; j++) {
-                System.out.print(array[j][row-i-1]+" ");
+            for (int j = i + 1; j <= row - 1 - i; j++) {
+                System.out.print(array[j][row - i - 1] + " ");
             }
-            for (int j = row-i-2; j >= i ; j--) {
-                System.out.print(array[row-i-1][j]+" ");
+            for (int j = row - i - 2; j >= i; j--) {
+                System.out.print(array[row - i - 1][j] + " ");
             }
-            for (int j = row-i-2; j > i ; j--) {
-                System.out.print(array[j][i]+" ");
+            for (int j = row - i - 2; j > i; j--) {
+                System.out.print(array[j][i] + " ");
             }
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number of Rows of matrics :");
@@ -39,6 +32,6 @@ public class SpiralMatrics {
                 Array[i][j] = sc.nextInt();
             }
         }
-        SpiralMatricsMaker(Array,row);
+        SpiralMatricsMaker(Array, row);
     }
 }
