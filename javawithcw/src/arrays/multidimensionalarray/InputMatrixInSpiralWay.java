41b380d86b6fb94880326f432ca6840570daf79a
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-21 16:02:48 +0530

    Taking input in Matrix in spiral order optimisely.

--- javawithcw/src/arrays/multidimensionalarray/InputMatrixInSpiralWay.java
+++ javawithcw/src/arrays/multidimensionalarray/InputMatrixInSpiralWay.java
@@ -3,42 +3,55 @@ package arrays.multidimensionalarray;
 import java.util.Scanner;
 
 public class InputMatrixInSpiralWay {
-    public static void PrintinfMatrixinSpiralOrder(int[][] array,int row) {
-        for (int i = 0; i <= row/2; i++) {
-            for (int j = i; j <= row-1-i; j++) {
-                System.out.print(array[i][j]+" ");
+    public static void PrintinfMatrixinSpiralOrder(int[][] array, int row) {
+        int left = 0, right = row - 1;
+        int top = 0, bottom = row - 1;
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                System.out.print(array[top][j] + " ");
             }
-            for (int j = i+1; j <= row-1-i ; j++) {
-                System.out.print(array[j][row-i-1]+" ");
+            top++;
+            for (int j = top; j <= bottom; j++) {
+                System.out.print(array[j][right] + " ");
             }
-            for (int j = row-i-2; j >= i ; j--) {
-                System.out.print(array[row-i-1][j]+" ");
+            right--;
+            for (int j = right; j >= left; j--) {
+                System.out.print(array[bottom][j] + " ");
             }
-            for (int j = row-i-2; j > i ; j--) {
-                System.out.print(array[j][i]+" ");
+            bottom--;
+            for (int j = bottom; j >= top; j--) {
+                System.out.print(array[j][left] + " ");
             }
+            left++;
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number of Rows of matrics :");
         int row = sc.nextInt();
         int[][] Array = new int[row][row];
         System.out.println("Enter the Elements of matrics ");
-        for (int i = 0; i <= row/2; i++) {
-            for (int j = i; j <= row-1-i; j++) {
-                Array[i][j]=sc.nextInt();
+        int left = 0, right = row - 1;
+        int top = 0, bottom = row - 1;
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                Array[top][j] = sc.nextInt();
             }
-            for (int j = i+1; j <= row-1-i ; j++) {
-                Array[j][row-i-1]=sc.nextInt();
+            top++;
+            for (int j = top; j <= bottom; j++) {
+                Array[j][right] = sc.nextInt();
             }
-            for (int j = row-i-2; j >= i ; j--) {
-                Array[row-i-1][j]=sc.nextInt();
+            right--;
+            for (int j = right; j >= left; j--) {
+                Array[bottom][j] = sc.nextInt();
             }
-            for (int j = row-i-2; j > i ; j--) {
-                Array[j][i]=sc.nextInt();
+            bottom--;
+            for (int j = bottom; j >= top; j--) {
+                Array[j][left] = sc.nextInt();
             }
+            left++;
         }
-        PrintinfMatrixinSpiralOrder(Array,row);
+        PrintinfMatrixinSpiralOrder(Array, row);
     }
 }
