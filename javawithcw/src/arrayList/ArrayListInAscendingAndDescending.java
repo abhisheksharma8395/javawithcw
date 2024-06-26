36461cd20925eace55f1a72645d918e9a78ba202
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-26 15:22:59 +0530

    Sorting ArrayList in ascending and descending order

--- javawithcw/src/arrayList/ArrayListInAscendingAndDescending.java
+++ javawithcw/src/arrayList/ArrayListInAscendingAndDescending.java
@@ -8,6 +8,7 @@ public class ArrayListInAscendingAndDescending {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> l1 = new ArrayList<>();
+        ArrayList<String> l2 = new ArrayList<>();
         l1.add(0);
         l1.add(10);
         l1.add(3);
@@ -18,5 +19,14 @@ public class ArrayListInAscendingAndDescending {
         System.out.println(l1);
         Collections.sort(l1,Collections.reverseOrder());
         System.out.println(l1);
+        l2.add("Welcome");
+        l2.add("To");
+        l2.add("PW");
+        l2.add("Skills");
+        System.out.println("Original List "+l2);
+        Collections.sort(l2);
+        System.out.println("List Sorted in Ascending Order "+l2);
+        Collections.sort(l1,Collections.reverseOrder());
+        System.out.println("List Sorted in Descending Order "+l2);
     }
 }
