3b48089a433f9d5508b3b37823b6ae830b0f6b1b
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-27 17:13:17 +0530

    revising recursion

--- javawithcw/src/recursion/FactorialByRecursion.java
+++ javawithcw/src/recursion/FactorialByRecursion.java
@@ -1,16 +1,15 @@
 package recursion;
-
 import java.util.Scanner;
-
 public class FactorialByRecursion {
-    public static int factorial(int n){
-        if(n==0) return 1;
-        return n*factorial(n-1);
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n =sc.nextInt();
+        int n = sc.nextInt();
         System.out.println(factorial(n));
     }
 }
