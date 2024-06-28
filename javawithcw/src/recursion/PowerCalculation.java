b25420e5fdadee15e813b36c7d9c80660dfd966f
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-28 13:03:30 +0530

    revising recursion

--- javawithcw/src/recursion/PowerCalculation.java
+++ javawithcw/src/recursion/PowerCalculation.java
@@ -2,10 +2,10 @@ package recursion;
 import java.util.Scanner;
 public class PowerCalculation {
     public static int PowerCalculaton(int num,int pow){
-        if(pow==0){
+        if(pow==0){                                //Base Case
             return 1;
         }
-        return num * PowerCalculaton(num,pow-1);
+        return num * PowerCalculaton(num,pow-1);  //Self Work + Recursive Condition
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
