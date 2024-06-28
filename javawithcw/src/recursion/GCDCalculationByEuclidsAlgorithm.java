4b3dc9e7f94c6e95dea5e6d98d9d68cb24bedd2d
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-28 15:45:41 +0530

    revising recursion

--- javawithcw/src/recursion/GCDCalculationByEuclidsAlgorithm.java
+++ javawithcw/src/recursion/GCDCalculationByEuclidsAlgorithm.java
@@ -3,8 +3,17 @@ package recursion;
 import java.util.Scanner;
 
 public class GCDCalculationByEuclidsAlgorithm {
+    public static int gcd(int val1,int val2,int helper){    //Brute Force Approach
+        if(helper==1){
+            return 1;
+        }
+        if(val1%helper==0 && val2%helper==0){
+            return helper;
+        }
+        return gcd(val1,val2,helper-1);
+    }
 
-    public static int eagcdr(int val1,int val2){
+    public static int eagcdr(int val1,int val2){  // Optimised Approach
         if(val1%val2==0){
             return val2;
         }
@@ -16,6 +25,8 @@ public class GCDCalculationByEuclidsAlgorithm {
         Scanner scanner = new Scanner(System.in);
         int val1 = scanner.nextInt();
         int val2 = scanner.nextInt();
+        int helper = Math.min(val1,val2);
         System.out.println(eagcdr(val1,val2));
+        System.out.println(gcd(val1,val2,helper));
     }
 }
