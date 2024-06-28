39151146eed6d62b3d0693df201960cf9cfd627f
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-28 12:38:46 +0530

    revising recursion

--- javawithcw/src/recursion/CountOfDigitsByRecursion.java
+++ javawithcw/src/recursion/CountOfDigitsByRecursion.java
@@ -1,15 +1,15 @@
 package recursion;
 import java.util.Scanner;
 public class CountOfDigitsByRecursion {
-    public static int countofdigits(int val){
-        if(val>=0 && val<=9){
-            return 1;
+    public static int CountOfDigits(int n){
+        if(n==0){
+            return 0;
         }
-        return 1+countofdigits(val/10);
+        return 1+CountOfDigits(n/10);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int val = sc.nextInt();
-        System.out.println(countofdigits(val));
+        System.out.println(CountOfDigits(val));
     }
 }
--- javawithcw/src/recursion/PowerCalculation.java
+++ javawithcw/src/recursion/PowerCalculation.java
@@ -1,16 +1,16 @@
 package recursion;
 import java.util.Scanner;
 public class PowerCalculation {
-    public static int powercalculation(int val,int pow){      //p is val and q is power
-        if(pow == 0){
+    public static int PowerCalculaton(int num,int pow){
+        if(pow==0){
             return 1;
         }
-        return val*powercalculation(val,pow-1);
+        return num * PowerCalculaton(num,pow-1);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int val = scanner.nextInt();
         int pow = scanner.nextInt();
-        System.out.println(powercalculation(val,pow));
+        System.out.println(PowerCalculaton(val,pow));
     }
 }
