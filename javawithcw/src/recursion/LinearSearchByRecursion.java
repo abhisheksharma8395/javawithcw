9e5002dfad3835ddb9d459ad401178e7041da5ea
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-29 13:46:17 +0530

    Revising Recursion.

--- javawithcw/src/recursion/LinearSearchByRecursion.java
+++ javawithcw/src/recursion/LinearSearchByRecursion.java
@@ -3,18 +3,16 @@ package recursion;
 import java.util.Scanner;
 
 public class LinearSearchByRecursion {
-    private static int linearsearch(int[]arr,int target,int idx){
-        //Base Case
-        if(idx == arr.length){
+    private static int LinearSearch(int[] arr, int target, int idx) {
+        if (idx == arr.length) {
             return -1;
         }
-        //Selfwork
-        if (arr[idx] == target){
+        if (arr[idx] == target) {
             return idx;
         }
-        //Subproblem
-        return linearsearch(arr,target,idx+1);
+        return LinearSearch(arr, target, idx + 1);
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -23,6 +21,6 @@ public class LinearSearchByRecursion {
             arr[i] = scanner.nextInt();
         }
         int target = scanner.nextInt();
-        System.out.println(linearsearch(arr,target,0));
+        System.out.println(LinearSearch(arr, target, 0));
     }
 }
