ce0e7bc5f82bfd0a669c4e6bc299ee0ec089e737
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-06-29 12:41:59 +0530

    revising recursion

--- javawithcw/src/recursion/SumOfTheArrayByUsingRecursion.java
+++ javawithcw/src/recursion/SumOfTheArrayByUsingRecursion.java
@@ -1,14 +1,15 @@
 package recursion;
+
 import java.util.Scanner;
+
 public class SumOfTheArrayByUsingRecursion {
-    private static int sumofarray(int[] arr,int idx){
-        //Base Case
-        if(idx == arr.length-1){
+    private static int SumOfArray(int[] arr, int idx) {
+        if (idx == arr.length - 1) {    // Base Case
             return arr[idx];
         }
-        //Subproblems + Self Work
-        return sumofarray(arr,idx+1)+arr[idx];
+        return arr[idx] + SumOfArray(arr, idx + 1);  // Self Work + Recursive Work
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -16,6 +17,6 @@ public class SumOfTheArrayByUsingRecursion {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-        System.out.println(sumofarray(arr,0));
+        System.out.println(SumOfArray(arr, 0));
     }
 }
