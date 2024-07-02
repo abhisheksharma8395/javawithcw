29aa388f2352daa0dd72804785958fb6b79c057e
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-07-02 14:28:29 +0530

    implementing bubble sort

---
+++ javawithcw/src/sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void Bsort(int[] array){
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1-i; j++) {
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i]=sc.nextInt();
+        }
+        Bsort(array);
+    }
+}
