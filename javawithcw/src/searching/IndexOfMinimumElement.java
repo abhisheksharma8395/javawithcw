67ed577bdc15bb0f4e2339966162bb4cfc789fef
abhisheksharma8395 <122424312+abhisheksharma8395@users.noreply.github.com>
2024-07-11 16:49:32 +0530

    finding the minimum element in rotated sorted array.

--- javawithcw/src/searching/IndexOfMinimumElement.java
+++ javawithcw/src/searching/IndexOfMinimumElement.java
@@ -9,7 +9,7 @@ public class IndexOfMinimumElement {
         }
         int mid = first + (last-first)/2;
         if (array[mid] <= array[array.length-1]) {   // 3 4 5 1 2 is rotated array if mid is at 2 index value = 5 which is greater than 2
-            return IndexMinimumElement(array,first,mid-1,mid); // than first = mid+1 now mid comes out 3 value = 1 1<2 ans updates its value to 1
+            return IndexMinimumElement(array,first,mid-1,mid); //  than first = mid+1 now mid comes out 3 value = 1 1<2 ans updates its value to 1
         }                                                       // last = mid-1 first index = 3 last index =2 loop breaks ans return
         return IndexMinimumElement(array, mid+1,last,ans);
     }
