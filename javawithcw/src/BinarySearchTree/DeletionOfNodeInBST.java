0b384272ca7aba2ad5f195591ad1cf622f182f5f
dell <abhiparashar8630@gmail.com>
2024-11-19 19:49:04 +0530

    updating function deleteNode

--- javawithcw/src/BinarySearchTree/DeletionOfNodeInBST.java
+++ javawithcw/src/BinarySearchTree/DeletionOfNodeInBST.java
@@ -25,8 +25,67 @@ public class DeletionOfNodeInBST {
         }
         return root;
     }
-    public static void DeleteNode(BinarySearchTreeNode root){
-        //delet
+    public static void delete(BinarySearchTreeNode root,int x){
+        if(root.val>x){
+            if(root.left==null) return;
+            else if(root.left.val==x){
+                //0 child node
+                if(root.left.left==null && root.left.right==null){
+                    root.left = null;
+                }
+                //1 child node
+                else if(root.left.left==null || root.left.right==null){
+                    if(root.left.left==null) root.left = root.left.right;
+                    else root.left = root.left.left;
+                }
+                //2 child node
+                else{
+                    BinarySearchTreeNode current = root.left;
+                    BinarySearchTreeNode pred = current.left;
+                    while(pred.right!=null){
+                        pred=pred.right;
+                    }
+                    delete(root,pred.val);
+                    pred.left = current.left;
+                    pred.right = current.right;
+                    root.left = pred;
+                }
+            }
+            else delete(root.left,x);
+        }
+        else{
+            if(root.right==null) return;
+            else if(root.right.val==x){
+                //0 child node
+                if(root.right.left==null && root.right.right==null){
+                    root.right = null;
+                }
+                //1 child node
+                else if(root.right.left==null || root.right.right==null){
+                    if(root.right.left==null) root.right = root.right.right;
+                    else root.right = root.right.left;
+                }
+                //2 child node
+                else{
+                    BinarySearchTreeNode current = root.right;
+                    BinarySearchTreeNode pred = current.right;
+                    while(pred.left!=null){
+                        pred=pred.left;
+                    }
+                    delete(root,pred.val);
+                    pred.left = current.left;
+                    pred.right = current.right;
+                    root.right = pred;
+                }
+            }
+            else delete(root.right,x);
+        }
+    }
+    public static BinarySearchTreeNode deleteNode(BinarySearchTreeNode root, int X) {
+        BinarySearchTreeNode temp =new BinarySearchTreeNode(Integer.MAX_VALUE);
+        temp.left = root;
+        delete(temp,X);
+        return temp.left;
     }
 
     public static void main(String[] args) {
@@ -34,6 +93,7 @@ public class DeletionOfNodeInBST {
         String[] arr = {"10", "5", "15", "2", "8", "12", "17"};
         BinarySearchTreeNode root = ConstructBST(arr);
         int target = sc.nextInt();
+        deleteNode(root,target);
 
     }
 
