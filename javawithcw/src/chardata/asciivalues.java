bd6baf8d43e439bb34d4183f65c2bc05aa806470
Abhishek Sharma <abhiparashar8630@gmail.com>
2024-12-03 23:05:47 +0530

    adding comments

--- javawithcw/src/chardata/asciivalues.java
+++ javawithcw/src/chardata/asciivalues.java
@@ -1,6 +1,7 @@
 package chardata;
 import java.util.Scanner;
 public class asciivalues {
+    //adding comments
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char ch = 'i';
