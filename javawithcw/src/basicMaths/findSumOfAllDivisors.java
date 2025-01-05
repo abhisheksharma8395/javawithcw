be817bdaf10ec0b73067ae5cb7b9fb585ff6f082
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-01-05 17:26:48 +0530

    finding sum of all divisors using optimising.

--- javawithcw/src/basicMaths/findSumOfAllDivisors.java
+++ javawithcw/src/basicMaths/findSumOfAllDivisors.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class findSumOfAllDivisors {
     public static int FindSumOfAllDivisors(int n) {
         int sum = 0;
-        int a = (int)Math.sqrt(n);
+        int a = (int) Math.sqrt(n);
         for (int i = 1; i <= a; i++) {
             if (n % i == 0) {
                 sum += i;
-                if(n/i!=i){
-                    sum += n/i;
+                if (n / i != i) {
+                    sum += n / i;
                 }
             }
 
         }
         return sum;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
